35600ab892de2852b35ed7a8dbf01808426d8e3e
Jonathan <jonsant0s@outlook.com>
2022-06-22 21:57:12 -0600

    RecordService.js expanded with more http requests

--- client/src/services/RecordService.js
+++ client/src/services/RecordService.js
@@ -1,11 +1,37 @@
 import http from "../http-common.js";
 
+const getAll = () => {
+    return http.get("/records");
+};
+
+const get = id => {
+    return http.get(`/records/${id}`);
+};
+
 const create = data => {
     return http.post("/records/create",data);
 };
 
+const update = (id, data) => {
+    return http.put(`/records/${id}`,data);
+}
+
+const remove = id => {
+    return http.delete(`/records/${id}`);
+};
+
+const findByTitle = title => {
+    return http.get(`/records?title=${title}`);
+}
+
+
 const RecordService = {
-    create
+    getAll,
+    get,
+    create,
+    update,
+    remove,
+    findByTitle
 };
 
 export default RecordService;
